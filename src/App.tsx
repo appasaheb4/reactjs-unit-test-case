eb113321bec77e4ee727db1331f0958199cae6ed
appasaheb4 <onlyappasaheb4@gmail.com>
2024-09-04 16:57:54 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">learn react</p>
+    </>
+  );
+}
+
+export default App;
