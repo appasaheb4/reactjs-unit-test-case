529bbf9ede07c35fbf917cfd7ebf6a07761f3dce
Appasaheb Lakade1 <Appasaheb.Lakade1@synechron.com>
2024-09-12 12:33:27 +0530

    test case modify'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import UserForm from './UserForm';
 import UserList from './UserList';
 
 function App() {
   const [users, setUsers] = useState<Array<any>>([]);
 
-  const onUserAdd = (user:any) => {
+  const onUserAdd = (user: any) => {
     setUsers([...users, user]);
   };
-
   return (
     <div>
       <UserForm onUserAdd={onUserAdd} />
